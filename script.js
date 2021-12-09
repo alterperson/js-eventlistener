0c90bd8367147ca2b458704ba9ef4e66468beb51
Vadim Melnichuk <vad.mk@ya.ru>
2021-12-09 21:33:23 +0300

    Added lesson11 code

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+const btn = document.querySelector('#btn');
+const input = document.querySelector('#text');
+const square = document.querySelector('#square');
 
+const eBtn = document.querySelector('#e_btn');
 
+const inputRange = document.querySelector('#range');
+const spanRange = document.querySelector('#range-span');
+const circle = document.querySelector('#circle');
 
+//===================1=====================
 
+btn.addEventListener('click', () => {
+  square.style.backgroundColor = `${input.value}`;
+})
 
+//===================2=====================
+
+eBtn.style.display = 'none'
+
+//===================3=====================
+
+inputRange.addEventListener('input', (event) => {
+  let number = event.target.value;
+  spanRange.textContent = number;
+  circle.style.width = `${number}%`;
+  circle.style.height = `${number}%`;
+})
